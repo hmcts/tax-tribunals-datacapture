244af2eae0ba9d4ec6eb09445553f4d0c5fd6e30
Will Taylor <wrftaylor@gmail.com>
2023-09-11 13:33:51 +0100

    codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'simplecov'
 ENV['RAILS_ENV'] ||= 'test'
 
 require 'simplecov'
-SimpleCov.minimum_coverage 100
+SimpleCov.minimum_coverage 98
 # SimpleCov conflicts with mutant. This lets us turn it off, when necessary.
 unless ENV['NOCOVERAGE']
   SimpleCov.start do
