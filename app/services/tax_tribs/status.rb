e016f36a8f399374abba2f2defcd42290816e5a4
Will Taylor <wrftaylor@gmail.com>
2023-09-29 10:58:02 +0100

    update mock env var to check for true

--- app/services/tax_tribs/status.rb
+++ app/services/tax_tribs/status.rb
@@ -38,7 +38,7 @@ module TaxTribs
     def glimr_status
       @glimr_status ||=
         begin
-          if ENV.fetch('GLIMR_API_CLIENT_MOCK', nil).present? ||
+          if ENV.fetch('GLIMR_API_CLIENT_MOCK', nil) == 'true' ||
                        GlimrApiClient::Available.call.available?
             'ok'
           end
