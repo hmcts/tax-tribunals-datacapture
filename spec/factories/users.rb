46e1d19c715d08b85653540a329d95b212359a4b
Will Taylor <wrftaylor@gmail.com>
2023-12-05 15:57:55 +0000

    begin upgrade

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :user do
-    id { '2c510a62-4ac8-4fad-acfd-d2b50b1c14f0' }
-    email { 'test@digital.justice.gov.uk' }
+    email { Faker::Internet.email }
     password { '!TaxTribun2897dxkjanwjk2a1' }
   end
 end
