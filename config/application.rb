6ca22cb07e25ee5b63212e990eac2fce36a0d6e1
Will Taylor <wrftaylor@gmail.com>
2024-01-22 11:15:15 +0000

    remove new security header

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-require_relative '../app/middleware/strict_transport_security_middleware'
+# require_relative '../app/middleware/strict_transport_security_middleware' # TEMP REMOVAL - 22 Jan
 
 require "rails"
 require "active_model/railtie"
@@ -71,7 +71,7 @@ module TaxTribunalsDatacapture
       config.middleware.use ApplicationInsights::Rack::TrackRequest, ENV['APP_INSIGHTS_INSTRUMENTATION_KEY']
     end
 
-    config.middleware.use StrictTransportSecurityMiddleware
+    # config.middleware.use StrictTransportSecurityMiddleware # TEMP REMOVAL - 22 Jan
 
     config.maintenance_enabled = ENV.fetch('MAINTENANCE_ENABLED', 'false').downcase == 'true'
     config.maintenance_allowed_ips = ENV.fetch('MAINTENANCE_ALLOWED_IPS', '').split(',').map(&:strip)
