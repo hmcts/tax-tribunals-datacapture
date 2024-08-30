de66ed01bcf858118faa15529c9df22757fe330c
Petr Zaparka <petr@zaparka.cz>
2024-08-30 15:15:10 +0100

    Fixing test db password

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,5 +1,5 @@
 DatabaseCleaner.strategy = :truncation
-DatabaseCleaner.url_allowlist = ['postgresql://postgres@db/tax-tribunals-datacapture']
+DatabaseCleaner.url_allowlist = ['postgresql://localhost@db/tt-datacapture_test']
 
 Before do
   DatabaseCleaner.clean
