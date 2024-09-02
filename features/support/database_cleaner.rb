4673cad1c962ca32ca6d91f5e771d0643078a100
Avi Singh <avi.singh@solirius.com>
2024-09-02 13:08:33 +0100

    updating database_cleaner, reverting to state where smoke tests run locally and on CI

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,6 +1,5 @@
 DatabaseCleaner.strategy = :truncation
-# DatabaseCleaner.url_allowlist = ['postgresql://postgres@localhost/tt-datacapture_test', 'postgres://postgres@localhost/tt-test-postgres']
-DatabaseCleaner.url_allowlist = ['postgresql://localhost@db/tt-datacapture_test']
+DatabaseCleaner.url_allowlist = ['postgresql://postgres@localhost/tt-datacapture_test', 'postgres://postgres@localhost/tt-test-postgres']
 
 Before do
   DatabaseCleaner.clean
