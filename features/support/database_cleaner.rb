e2e7e460ceb5ec8205ee489b4fab32c9c1836d1b
Avi Singh <avi.singh@solirius.com>
2024-09-02 12:00:17 +0100

    fixing smoke tests

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,5 +1,5 @@
 DatabaseCleaner.strategy = :truncation
-DatabaseCleaner.url_allowlist = ['postgresql://localhost@db/tt-datacapture_test']
+DatabaseCleaner.url_allowlist = ['postgresql://postgres@localhost/tt-datacapture_test', 'postgres://postgres@localhost/tt-test-postgres']
 
 Before do
   DatabaseCleaner.clean
