ba9628a1e93ca8fd59f2db02933e16d4f155d4b7
Avi Singh <avi.singh@solirius.com>
2024-09-02 12:38:25 +0100

    updating database_cleaner again, second attempt to fix build issue

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,6 +1,6 @@
 DatabaseCleaner.strategy = :truncation
 # DatabaseCleaner.url_allowlist = ['postgresql://postgres@localhost/tt-datacapture_test', 'postgres://postgres@localhost/tt-test-postgres']
-DatabaseCleaner.url_allowlist = ['postgresql://postgres@localhost/tt-datacapture_test']
+DatabaseCleaner.url_allowlist = ['postgresql://localhost@db/tt-datacapture_test']
 
 Before do
   DatabaseCleaner.clean
