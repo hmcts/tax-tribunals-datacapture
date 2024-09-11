ffb429164db3b2c3a6a8bd45c80b286c46a3165b
Azarren <daniellekushnir22@gmail.com>
2024-09-11 11:01:50 +0100

    RST-6843 - Updated govuk-frontend to 4.5.0

--- features/support/page_objects/guidance_page.rb
+++ features/support/page_objects/guidance_page.rb
@@ -5,9 +5,9 @@ class GuidancePage < BasePage
     element :header, 'h1', text: I18n.t('guidance.heading')
     element :visible_first_question, '#accordion-1-heading-0', visible: true
     element :visible_second_question, '#accordion-1-heading-1', visible: true
-    element :visible_first_answer, '#accordion-1-content-0', visible: true
-    element :visible_second_answer, '#accordion-1-content-1', visible: true
-    element :open_all, '#accordion-1 .govuk-accordion__open-all'
+    element :visible_first_answer, '#accordion-1-content-0', visible: :all
+    element :visible_second_answer, '#accordion-1-content-1', visible: :all
+    element :open_all, '#accordion-1 .govuk-accordion__show-all-text'
   end
 
   def click_a_question
