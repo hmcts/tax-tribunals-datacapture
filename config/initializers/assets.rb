aab8ba5df83a10c93a4e490b093507865938e8d0
Azarren <daniellekushnir22@gmail.com>
2024-09-17 13:30:44 +0100

    RST-6843 - Undo wildcard removal in precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,13 @@
 Rails.application.config.assets.version = '1.1'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w[*.png *.ico]
+Rails.application.config.assets.precompile += %w[.svg .eot .woff .ttf .woff2 .woff]
+Rails.application.config.assets.precompile += %w(
+  apple-touch-icon.png
+  apple-touch-icon-180x180.png
+  apple-touch-icon-167x167.png
+)
+
 Rails.application.config.assets.precompile += %w(
   govuk-frontend/dist/govuk/all.scss
   govuk-frontend/dist/govuk/all.bundle.js
