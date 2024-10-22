211bcd03e8730741e4933a2f4e1360105a37d827
Azarren <daniellekushnir22@gmail.com>
2024-10-22 16:11:47 +0100

    RST-6892 - Remove unnecessary comments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,10 +43,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
-  # config.assume_ssl = true
-
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
   config.ssl_options = {
