4f8ea13057ec58d0442cecae97a39f8334a4d6c1
Aaron Lambley <aaron.lambley@gmail.com>
2024-11-07 14:21:37 +0000

    Move initial form saves to after language selection step

--- app/controllers/steps/details/user_type_controller.rb
+++ app/controllers/steps/details/user_type_controller.rb
@@ -1,5 +1,8 @@
 module Steps::Details
   class UserTypeController < Steps::DetailsStepController
+    # steps/details/user_type is the first step where closure should be saved
+    include StartingPointStep
+
     def edit
       @form_object = UserTypeForm.new(
         tribunal_case: current_tribunal_case,
@@ -9,6 +12,12 @@ module Steps::Details
 
     def update
       update_and_advance(UserTypeForm)
+    end
+
+    private
+
+    def intent
+      Intent::CLOSE_ENQUIRY
     end
   end
 end
