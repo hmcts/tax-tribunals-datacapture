15d7cd995b00e7e1ec87cd683c96db90610c7947
Petr Zaparka <petr@zaparka.cz>
2024-11-14 10:14:10 +0000

    Fixing cookies banner feature test

--- features/step_definitions/cookie_steps.rb
+++ features/step_definitions/cookie_steps.rb
@@ -1,19 +1,19 @@
 Then("I see the cookie banner is present") do
-  expect(home_page.cookie).to have_header
-  expect(home_page.cookie).to have_accept_button
-  expect(home_page.cookie).to have_reject_button
+  expect(home_page.cookie_banner).to have_header
+  expect(home_page.cookie_banner).to have_accept_button
+  expect(home_page.cookie_banner).to have_reject_button
 end
 
 When("I accept the cookies") do
-  home_page.cookie.accept_button.click
+  home_page.cookie_banner.accept_button.click
 end
 
 When("I reject the cookies") do
-  home_page.cookie.reject_button.click
+  home_page.cookie_banner.reject_button.click
 end
 
 Then("I see the cookie banner is not present") do
-  expect(home_page.cookie).to have_no_header
+  expect(home_page.cookie_banner).to have_no_header
 end
 
 And("I click save settings") do
