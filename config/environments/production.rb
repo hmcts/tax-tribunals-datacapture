6ee0d0e050e27673277e498ae7fc5314e7954242
sgmselli <sellings.matthew@gmail.com>
2024-12-17 15:57:26 +0000

    remove prefix for sidekiq queue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :sidekiq
-  config.active_job.queue_name_prefix = "tax_tribunals_datacapture_production"
-
 
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
