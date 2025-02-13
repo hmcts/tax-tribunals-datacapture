b31ccae674f5f90d58ddcf9841570be5a96c849f
sgmselli <sellings.matthew@gmail.com>
2025-02-13 16:05:14 +0000

    Add sentry loggers to monitor application submission

--- app/services/tax_tribs/case_creator.rb
+++ app/services/tax_tribs/case_creator.rb
@@ -7,12 +7,19 @@ module TaxTribs
     end
 
     def call
+
+      Sentry.capture_message("INFO: Submitting application in progress")
+
       tribunal_case.update(
         case_status: CaseStatus::SUBMIT_IN_PROGRESS
       )
 
+      Sentry.capture_message("INFO: Before GLiMR")
+
       glimr_case = GlimrNewCase.new(tribunal_case).call
       case_reference = glimr_case.case_reference
+
+      Sentry.capture_message("INFO: After GLiMR")
 
       # case_reference could be nil, if GLiMR call failed, but despite this,
       # we want to mark the tribunal case as `submitted`
@@ -21,6 +28,8 @@ module TaxTribs
         submitted_at: Time.zone.now,
         case_status: CaseStatus::SUBMITTED
       )
+
+      Sentry.capture_message("INFO: Submitted")
     end
   end
 end
