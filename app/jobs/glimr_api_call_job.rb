8bb8750b996935b046849f042017f6c845a776ec
sgmselli <sellings.matthew@gmail.com>
2025-02-24 11:02:28 +0000

    Add error handling to Glimr Job

--- app/jobs/glimr_api_call_job.rb
+++ app/jobs/glimr_api_call_job.rb
@@ -1,5 +1,6 @@
 class GlimrApiCallJob < ApplicationJob
   queue_as :glimr_api_calls
+  retry_on StandardError, wait: 5.seconds, attempts: 3
 
   def perform(tribunal_case)
     glimr_case = TaxTribs::GlimrNewCase.new(tribunal_case).call
@@ -9,10 +10,9 @@ class GlimrApiCallJob < ApplicationJob
       case_reference:
     )
 
-    Sentry.capture_message("DEBUG: Sidekiq job executed")
-    # rescue StandardError => e
-    #   Rails.logger.info({ caller: self.class.name, method: __callee__, error: e }.to_json)
-    #   Sentry.capture_exception(e, extra: { tribunal_case_id: tribunal_case.id })
-    # on fail can send email to user saying that their case wasn't successfully sent
+  rescue StandardError => e
+    Rails.logger.info({ caller: self.class.name, method: __callee__, error: e }.to_json)
+    Sentry.capture_exception(e, extra: { tribunal_case_id: tribunal_case.id })
+    raise e
   end
 end
