651b2d8ec136cb04f9828282a09ec77a82af1ed8
sgmselli <sellings.matthew@gmail.com>
2025-02-27 10:24:34 +0000

    Remove debugging logs

--- app/jobs/glimr_api_call_job.rb
+++ app/jobs/glimr_api_call_job.rb
@@ -3,18 +3,13 @@ class GlimrApiCallJob < ApplicationJob
   sidekiq_options retry: 3
 
   def perform(tribunal_case)
-    Rails.logger.info("DEBUG: BEFORE CALL")
-
     glimr_case = TaxTribs::GlimrNewCase.new(tribunal_case).call
-
-    Rails.logger.info("DEBUG: AFTER CALL")
 
     case_reference = glimr_case.case_reference
 
     tribunal_case.update(
       case_reference:
     )
-    Rails.logger.info("DEBUG: AFTER CASE REFERENCE: #{case_reference}")
   rescue StandardError => e
     Sentry.capture_exception(e, extra: { tribunal_case_id: tribunal_case.id })
     Rails.logger.info({ caller: self.class.name, method: __callee__, error: e }.to_json)
