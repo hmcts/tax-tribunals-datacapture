25d50116289fbc8318f17ffb276d63fb6caf588b
Petr Zaparka <petr@zaparka.cz>
2025-03-13 14:33:31 +0000

    Cop refactor

--- app/controllers/employees/sessions_controller.rb
+++ app/controllers/employees/sessions_controller.rb
@@ -5,7 +5,6 @@ class Employees::SessionsController < Devise::SessionsController
 
   # GET /resource/sign_in
   # def new
-  #   # binding.pry
   #   super
   # end
 
@@ -19,22 +18,4 @@ class Employees::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  protected
-  # def devise_parameter_sanitizer
-  #   # binding.pry
-  #   # :a
-  #   EmployeeParameterSanitizer.new(Employee, :employee, params)
-  #   # params.permit(employee_signin: [:email, :password])
-  #   # if resource_class == User
-  #   #   User::ParameterSanitizer.new(User, :user, params)
-  #   # else
-  #   #   super # Use the default one
-  #   # end
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   binding.pry
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
