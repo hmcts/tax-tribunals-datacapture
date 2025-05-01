fe6b569bc8be4cec4b7a170985acefb92189162b
kalvinlyu <kalvin.lyu@solirius.com>
2025-05-01 16:25:14 +0100

    test fix screenshot

--- features/support/capybara_driver_helper.rb
+++ features/support/capybara_driver_helper.rb
@@ -22,7 +22,7 @@ Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
-Capybara::Screenshot.register_driver(:headless) do |driver, path|
+Capybara::Screenshot.register_driver(:firefox) do |driver, path|
   driver.browser.save_screenshot(path)
 end
 
