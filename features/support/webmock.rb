f7cab199eee5bb4e34ee061eb00084a1f7c75bfa
DanielleKushnir <daniellekushnir22@gmail.com>
2025-05-02 11:40:01 +0100

    Webmock

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,2 +1,2 @@
-WebMock.stub_request(:get, "https://www.gov.uk/tax-appeals")
+WebMock.stub_request(:get, "www.gov.uk")
        .to_return(status: 200, body: '<html><body>Disagree with a tax decision</body></html>', headers: {})
