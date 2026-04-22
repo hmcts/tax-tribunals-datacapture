a778453fd0d3c3bdaafd09cce9b0918f94cf3c7e
markherbertcgi <mark.herbert@hmcts.net>
2026-04-22 15:33:19 +0100

    change select_language_controller.rb back

--- app/controllers/steps/select_language_controller.rb
+++ app/controllers/steps/select_language_controller.rb
@@ -1,11 +1,8 @@
 module Steps
   class SelectLanguageController < StepController
     skip_before_action :check_tribunal_case_presence
-    skip_before_action :check_tribunal_case_status
 
     def edit
-      redirect_to invalid_session_errors_path unless current_tribunal_case
-
       @form_object = SelectLanguage::SaveLanguageForm.new(
         tribunal_case: current_tribunal_case,
         language: current_tribunal_case.language
@@ -13,14 +10,13 @@ module Steps
     end
 
     def update
-      redirect_to invalid_session_errors_path unless current_tribunal_case
       update_and_advance(SelectLanguage::SaveLanguageForm)
     end
 
     private
 
     def decision_tree_class
-      if current_tribunal_case&.tax_appeal?
+      if current_tribunal_case.tax_appeal?
         AppealDecisionTree
       else
         TaxTribs::ClosureDecisionTree
