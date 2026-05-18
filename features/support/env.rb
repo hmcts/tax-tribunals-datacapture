7a3a16871cb3df4fd9f27ff384aaeb718dc157ce
markherbertcgi <mark.herbert@hmcts.net>
2026-05-18 15:01:33 +0100

    downgrade puma to 7.2.0

--- features/support/env.rb
+++ features/support/env.rb
@@ -57,12 +57,12 @@ Capybara.javascript_driver = :selenium
 Capybara.raise_server_errors = false
 
 ENV['NO_PROXY'] = ENV['no_proxy'] = '127.0.0.1'
-if ENV['APP_HOST']
- Capybara.app_host = ENV['APP_HOST']
- if Capybara.app_host.chars.last != '/'
-   Capybara.app_host += '/'
- end
-end
+#if ENV['APP_HOST']
+#Capybara.app_host = ENV['APP_HOST']
+#if Capybara.app_host.chars.last != '/'
+#   Capybara.app_host += '/'
+# end
+#end
 
 if ENV['TEST_LOCALE'] == "cy"
   I18n.locale = 'cy'
