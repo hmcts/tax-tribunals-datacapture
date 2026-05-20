8b82f9954a9a0d345dbdfd1c78b13a7a6c33db95
Neil Perry <neil.perry@cgi.com>
2026-05-20 14:08:42 +0100

    allow smoke tests on remote db

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,5 +1,13 @@
 DatabaseCleaner.strategy = :truncation
-DatabaseCleaner.url_allowlist = ['postgresql://postgres@localhost/tt-datacapture_test', 'postgres://postgres@localhost/tt-test-postgres']
+
+preview_database = ENV['DB_HOST'] == 'tax-tribunals-preview.postgres.database.azure.com' &&
+                   ENV['DB_NAME'].to_s.match?(/\Apr-\d+\z/)
+
+if preview_database
+  DatabaseCleaner.allow_remote_database_url = true
+else
+  DatabaseCleaner.url_allowlist = ['postgresql://postgres@localhost/tt-datacapture_test', 'postgres://postgres@localhost/tt-test-postgres']
+end
 
 Before do
   DatabaseCleaner.clean
